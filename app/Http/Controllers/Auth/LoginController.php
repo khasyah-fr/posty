befdb80b669c07fff716b3dc55075284569ff135
FtrR <bytenauts@gmail.com>
2020-11-25 22:15:53 +0700

    Initial commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function store(Request $request)
+    {
+        //Validate request, catches an exception and redirect automatically if not valid
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        //Sign user in
+        if(!auth()->attempt($request->only('email', 'password'))){
+            return back()->with('status', 'Invalid login details');
+        }
+
+        //Redirect
+        return redirect()->route('dashboard');
+    }
+}
---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function index()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        //Validate request, catches an exception and redirect automatically if not valid
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed',
+        ]);
+
+        //Store user
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        //Sign user in
+        auth()->attempt($request->only('email', 'password'));
+
+        //Redirect
+        return redirect()->route('dashboard');
+    }
+}
